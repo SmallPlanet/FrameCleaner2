aebd68924d9c445af2e1981148e95c1dd2bf1ef2
Quinn McHenry <quinn.mchenry@smallplanet.com>
2014-07-07 16:35:58 -0400

    Initial commit, starting to build out document xib

---
+++ FrameCleaner/SPDocument.h
@@ -0,0 +1,13 @@
+//
+//  SPDocument.h
+//  FrameCleaner
+//
+//  Created by Quinn McHenry on 7/7/14.
+//  Copyright (c) 2014 Small Planet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SPDocument : NSDocument
+
+@end
---
+++ FrameCleaner/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FrameCleaner
+//
+//  Created by Quinn McHenry on 7/7/14.
+//  Copyright (c) 2014 Small Planet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
